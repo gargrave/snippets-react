5774afb01732dac2c909ef023fa5ba3221dd9a2e
Gabe Hargrave <rinkrinkerfink@gmail.com>
2016-08-24 08:12:45 -0700

    Remove unnecessary HomePage

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,10 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './App';
-import HomePage from './modules/common/containers/HomePage';
 import AboutPage from './modules/common/containers/AboutPage.js';
 import NotFoundPage from './modules/common/containers/NotFoundPage.js';
 
-import SnippetsListPage from './modules/snippets/containers/SnippetsListPage';
+import SnippetsListPageDefault from './modules/snippets/containers/SnippetsListPage';
 import SnippetCreatePage from './modules/snippets/containers/SnippetCreatePage';
 import SnippetDetailPage from './modules/snippets/containers/SnippetDetailPage';
 
@@ -17,12 +16,12 @@ import CreateAccountPage from './modules/account/containers/CreateAccountPage';
 
 export default (
   <Route path="/" component={App}>
-    <IndexRoute component={SnippetsListPage}/>
+    <IndexRoute component={SnippetsListPageDefault}/>
 
-    <Route path="snippets" component={SnippetsListPage}/>
+    <Route path="snippets" component={SnippetsListPageDefault}/>
     <Route path="snippets/new" component={SnippetCreatePage}/>
     <Route path="snippets/:id" component={SnippetDetailPage}/>
-    <Route path="snippets/filter/:filterBy" component={SnippetsListPage}/>
+    <Route path="snippets/filter/:filterBy" component={SnippetsListPageDefault}/>
 
     <Route path="account" component={AccountPage}/>
     <Route path="account/login" component={LoginPage}/>
