c986df3a7bbfa1c1b5366a5734d5f5b9b6ff31ff
Gabe Hargrave <rinkrinkerfink@gmail.com>
2016-08-24 09:54:20 -0700

    Add color display to SnippetListDetail

--- src/modules/snippets/components/SnippetListDetail.js
+++ src/modules/snippets/components/SnippetListDetail.js
@@ -2,15 +2,46 @@ import React, {PropTypes} from 'react';
 
 
 const SnippetListDetail = ({snippet, gotoDetailPage, onStarClick}) => {
+  let panelColorStyle = '';
+  switch (snippet.color) {
+    case 'white':
+      panelColorStyle = 'snippet-color-white';
+      break;
+    case 'red':
+      panelColorStyle = 'snippet-color-red';
+      break;
+    case 'green':
+      panelColorStyle = 'snippet-color-green';
+      break;
+    case 'blue':
+      panelColorStyle = 'snippet-color-blue';
+      break;
+    case 'yellow':
+      panelColorStyle = 'snippet-color-yellow';
+      break;
+    case 'orange':
+      panelColorStyle = 'snippet-color-orange';
+      break;
+    case 'teal':
+      panelColorStyle = 'snippet-color-teal';
+      break;
+    case 'gray':
+      panelColorStyle = 'snippet-color-gray';
+      break;
+    default:
+      panelColorStyle = 'snippet-color-white';
+      break;
+  }
+  let panelFullStyle = `panel panel-default ${panelColorStyle}`;
 
   return (
-    <div className="panel panel-default">
+    <div className={panelFullStyle}>
       <div className="panel-body">
-        <h4>{snippet.title || 'Untitled Snippet'}</h4>
-        <a href={snippet.url} className="text-muted" target="_blank">{snippet.url}</a>
+        <h4 className="snippet-title">{snippet.title || 'Untitled Snippet'}</h4>
+        <a href={snippet.url} className="text-muted snippet-url" target="_blank">{snippet.url}</a>
       </div>
 
-      <div className="panel-footer">
+      <div className="panel-footer snippet-controls">
         {/* star/unstar button */}
         {snippet.starred &&
           <span onClick={onStarClick}>&nbsp;
