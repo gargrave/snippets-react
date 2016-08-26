7c7b5307914e194d3980f3e6598250c24188e823
Gabe Hargrave <rinkrinkerfink@gmail.com>
2016-08-26 14:21:03 -0700

    Tests for NewSnippetPanel

---
+++ test/client/snippets/components/NewSnippetPanel.spec.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import {expect} from 'chai';
+import sinon from 'sinon';
+
+import firebaseConfig from '../../../../src/etc/firebaseConfig';
+import NewSnippetPanel from '../../../../src/modules/snippets/components/NewSnippetPanel';
+
+
+describe('<NewSnippetPanel />', () => {
+  const props = {
+    onPanelClick: sinon.spy(),
+  };
+
+  it('should have one main element with the appropriate class', () => {
+    const wrapper = shallow(<NewSnippetPanel {...props} />);
+    const mainNode = wrapper.find('.new-snippet-panel');
+
+    expect(mainNode).to.have.length(1);
+  });
+
+  it('should call the click handler', () => {
+    const wrapper = shallow(<NewSnippetPanel {...props} />);
+
+    wrapper.simulate('click');
+    expect(props.onPanelClick).to.have.property('callCount', 1);
+  });
+});
